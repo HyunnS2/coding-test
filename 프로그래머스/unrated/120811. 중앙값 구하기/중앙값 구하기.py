7b788a76916f2708f0b96aba23e63a9238b8ffbd
JeongHyun <105518951+HyunnS2@users.noreply.github.com>
2023-12-29 14:31:42 +0900

    [level unrated] Title: 중앙값 구하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.py
+++ 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.py
@@ -1,8 +1,13 @@
-def solution(array):
-    
-    
+def solution2(array):
     array.sort()
     m = len(array)//2
-    
-    
     return array[m]
+
+
+def solution(array):
+    answer = 0
+    array.sort()
+    length = len(array)
+    m = int(length/2)
+    answer = array[m]
+    return answer
