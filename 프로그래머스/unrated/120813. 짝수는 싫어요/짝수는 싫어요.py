b6bf270766063fd3ffdc5643583798ee454cb6d1
JeongHyun <105518951+HyunnS2@users.noreply.github.com>
2023-12-29 14:32:06 +0900

    [level unrated] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -1,10 +1,18 @@
-def solution(n):
+def solution2(n):
     answer = []
-    
     for i in range(1,n+1):
-        
         if i%2!=0:
             answer.append(i)
-            answer.sort()
-    
     return answer
+
+def solution3(n):
+    answer = []
+    for i in range(1,n+1, 2):
+        answer.append(i)
+    return answer
+
+
+def solution(n):
+    return [i for i in range(1,n+1, 2)]
+
+
